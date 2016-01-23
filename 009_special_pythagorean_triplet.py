210434cab27768667618871449bf0ee49709304c
Justin Bassett <justin@reddit.com>
2016-01-23 15:33:25 -0800

    adds solution for 009
    adds a solution for problem 009, finding the pythagorean triple that
    sums to a given number

---
+++ 009_special_pythagorean_triplet.py
@@ -0,0 +1,14 @@
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000
+# Find the product abc
+
+def pythag_triple_sum(sumNum):
+	for a in range(1, sumNum/3):
+		for b in range(a+1, sumNum/2):
+			c = sumNum - a - b
+			if a*a + b*b == c*c:
+				return a*b*c
+print pythag_triple_sum(1000)
